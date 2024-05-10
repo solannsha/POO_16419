8196784a4f6acfc5045bf64dbe090a5a5957aeda
MezaJose12 <jameza1@espe.edu.ec>
2024-05-10 08:08:10 -0500

    Grupo 5 Funcion BOOL

--- Deberes/corazon3.cpp
+++ Deberes/corazon3.cpp
@@ -3,47 +3,49 @@ PROBLEMA: MAIN CON FUNCIONES Y SUBIR A GITHUB
 AUTOR: NRC16419
 FECHA DE CREACION: 09/05/2024
 FECHA DE MODIFICACION: 10/05/2024*/
-#include <iostream>;
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-
 int suma(int a, int b) {
-	return a + b;
+    return a + b;
 }
 
-<<<<<<< HEAD
-// grupo 2 agrga la Función de proceso
+float division(int a, int b) {
+    return a / b;
+}
+
 double proceso(int rs, float rd, int a, int b) {
     return rs * rd * a * b;
 }
 
-=======
-
-
-float division(int a, int b){
-	return a/b;
+bool verificarDivision(float a) {
+    return a > M_PI;
 }
-void imprimirResultados(int rs, float rd) {
-  cout << "Suma: " << rs << endl;
-  cout << "Division: " << rd << endl;
+
+void imprimirResultados(int rs, float rd, bool sw, double rp) {
+    cout << "Suma: " << rs << endl;
+    cout << "Division: " << rd << endl;
+    if (sw) {
+        cout << "La division es mayor a pi." << endl;
+    } else {
+        cout << "La division no es mayor a pi." << endl;
+    }
 }
->>>>>>> 53a8879efdc43ccf91a2949baede95d8efe6b9bc
+
 int main() {
-int rs;
-float rd;
-bool sw;
+    int rs;
+    float rd;
+    bool sw = false;
+    double rp;
 
-char valor [20];
-double rp;
-  rs=suma(5,6);
-  rs = suma(5, 6);
-  rd = 5.0 / 6; 
-  
-  imprimirResultados(rs,rd);
-  
-  
-  return 0;
-  
-  
+    rs = suma(5, 6);
+    rd = 5.0 / 6;
+    sw = verificarDivision(rd);
+    rp = proceso(rs, rd, 5, 6);
+
+    imprimirResultados(rs, rd, sw, rp);
+
+    return 0;
 }
 
