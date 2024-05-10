a0526d6629223b7a2a00d65e608213d4ff797470
solannsha <savillegas2@espe.edu.ec>
2024-05-09 19:10:10 -0500

    aunmente 5 int main

---
+++ Deberes/corazon1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>;
+using namespace std;
+
+int main() {
+int rs;
+float rd;
+bool sw;
+char valor [20];
+double rp;
+  rs=suma(5,6);
+  rd=division(5,6);
+  rp=proceso(rs,rd,3,33);
+  sw=verificar(rp);
+  string datos_str = "15 suma"; 
+  strcpy(valor, datos_str.c_str()); 
+  cout<<rs<<endl;
+  cout<<rd<<endl;
+  cout<<rp<<endl;
+  cout<<sw<<endl;
+  cout<<valor<<endl;
+  
+  
+}
