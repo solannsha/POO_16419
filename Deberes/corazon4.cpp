7979031111f24f60c00b1bb756940fc93c53206f
kvnrls21 <karuales@espe.edu.ec>
2024-05-10 02:22:29 -0500

    Procesos
    Creacion Funcion 3

--- Deberes/corazon4.cpp
+++ Deberes/corazon4.cpp
@@ -1,6 +1,19 @@
 #include <iostream>;
 using namespace std;
 
+double proceso(int rs, float rd, int a, int b);
+//G4: 3era Func, 5to Programa
+double proceso(int rs, float rd, int a, int b){
+	double resp1, resp2;
+	if (rd!=0){
+		resp1= 4*(rs-a)+(5+b/rd);
+		return resp1;
+	}else {
+		resp2=-7*(b+rd)+4*(a*rs+2);
+		return resp2;
+	}
+}
+
 int main() {
 int rs;
 float rd;
