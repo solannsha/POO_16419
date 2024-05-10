feb3fb430f79d4000b7979898d87a9d7b2b3fed2
Jelen04 <jdlucio@espe.edu.ec>
2024-05-10 08:41:16 -0500

    grupo 2 modifica corazon 2

--- Deberes/corazon2.cpp
+++ Deberes/corazon2.cpp
@@ -4,7 +4,7 @@ AUTOR: NRC16419
 FECHA DE CREACION: 09/05/2024
 FECHA DE MODIFICACION: 10/05/2024*/
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -18,8 +18,12 @@ int suma(int a, int b) {
 void imprimirResultados(int rs,float rd) {
   cout << "Suma: " << rs << endl;
   cout<<"la division es:"<<rd<<endl;
+  
 }
-
+//grupo 2 agrega funcion para manejar caracteres 
+void manejarCadena(char* valor, const string& datos_str) {
+    strcpy(valor, datos_str.c_str());
+}
 
 int main() {
   int rs;
@@ -31,8 +35,12 @@ int main() {
   rs = suma(5, 6);
   rd= division(7.8,8.9);
   imprimirResultados(rs,rd);
+  string datos_str= ("15 Suma");
+  manejarCadena(valor, datos_str);
+  cout << valor << endl;
   
 
+  
   return 0;
 }
 
