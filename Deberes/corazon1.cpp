2ffe2659fe4ecfe67439f2b8412cdb6a3244f80f
MezaJose12 <jameza1@espe.edu.ec>
2024-05-10 07:17:41 -0500

    Grupo 5 sube la funcion suma

--- Deberes/corazon1.cpp
+++ Deberes/corazon1.cpp
@@ -4,13 +4,18 @@ AUTOR: NRC16419
 FECHA DE CREACION: 09/05/2024
 FECHA DE MODIFICACION: 10/05/2024*/
 #include <iostream>;
+#include <string>
 using namespace std;
+
+//Grupo 5 Funcion suma
+int suma(int a, int b);
 
 float division(int a, float b){
   return a/b;
 }
 
-void imprimirResultados(bool sw, const char* valor) {
+void imprimirResultados(int rs,bool sw, const char* valor) {
+    cout<<"La suma es Suma:"<<endl;
     cout << "Verificación: " << (sw ? "verdadero" : "falso") << endl;
     cout << "Valor: " << valor << endl;
 }
@@ -27,24 +32,27 @@ float rd;
 bool sw;
 char valor [20];
 double rp;
-  rs=suma(5,6);
+  rs=suma(48,79);
   rd=division(5,6);
   rp=proceso(rs,rd,3,33);
   sw=verificar(rp);
   string rp_str = doubleToString(rp);
   string datos_str = rp_str + " proceso";
-   strncpy(valor, datos_str.c_str(), sizeof(valor)); 
-   
-   valor[sizeof(valor) - 1] = '\0';
+  strncpy(valor, datos_str.c_str(), sizeof(valor));  
+  valor[sizeof(valor) - 1] = '\0';
   
   cout<<rs<<endl;
   cout<<rd<<endl;
   cout<<rp<<endl;
   cout << boolalpha << sw << endl; 
   cout<<valor<<endl;
-  imprimirResultados(sw, valor);
+  imprimirResultados(rs,sw, valor);
 
     return 0;
   
   
+}
+
+int suma(int a, int b){
+  return a+b;
 }
