945ea8e8d36983593b45bb475f8052a154e4a9b6
Domitorius <djcaiza6@espe.edu.ec>
2024-05-09 22:00:10 -0500

    comentario2

--- Deberes/corazon3.cpp
+++ Deberes/corazon3.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
-int c=5, d=9;
-bool comparador = c==d;
+#include <iostream>;
+using namespace std;
+
 int main() {
-    std::cout << comparador;
-return 0;
+int rs;
+float rd;
+bool sw;
+char valor [20];
+double rp;
+  rs=suma(5,6);
+  rd=division(5,6);
+  rp=proceso(rs,rd,3,33);
+  sw=verificar(rp);
+  string datos_str = "15 suma"; 
+  strcpy(valor, datos_str.c_str()); 
+  cout<<rs<<endl;
+  cout<<rd<<endl;
+  cout<<rp<<endl;
+  cout<<sw<<endl;
+  cout<<valor<<endl;
+  
+  
 }
